496a529e410d916cd5321b555d5fbc85e4254a65
tankakatan <d.n.voropaev@gmail.com>
2017-01-15 20:40:17 +0300

    Initial Commit

---
+++ TCPClientC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TCPClientC
+//
+//  Created by Денис on 15/01/2017.
+//  Copyright © 2017 Den V Design. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
